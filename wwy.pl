45de479f1e6e7947c9f6c05f9da9d646a0621ad2
willtent <wjt9603@rit.edu>
2013-10-31 15:31:21 -0400

    Perl Script

---
+++ wwy.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+#Things to be achieved:
+#++++warning alllll Psudocode
+
+#1. Take in an Array of jpeg or png Files (command line entered) have abilit to enter a
+
+#directory and use all files in directory
+
+#2. sub to do a file crawl through direcotry and some way to tell whether attributes are files
+
+#or directories
+
+#3. pipe files to jhead and save data to files
+
+#4. scan and seperate gps cordinates from file
+
+#5. modify cordinate inforamtion to work for google maps (convert to decimal)
+
+
+#----------------------------------------------------------
+#6. create a bread crumb trail in google maps
+#----------------------------------------------------------
