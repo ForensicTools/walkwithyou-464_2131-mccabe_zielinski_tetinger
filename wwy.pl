767ea0c3b3d92317732c18b9996e6be954918dc8
rpm4504 <rpm4504@rit.edu>
2013-11-21 14:36:04 -0500

    Working wwy.pl

--- wwy.pl
+++ wwy.pl
@@ -33,32 +33,39 @@ my @longitudes;	#Array for GPS longitude of photo
 foreach my $file (@ARGV) {
 	#Read jhead information into temp array
 	@tmpArr = `jhead $file`;
-		
+	
 	#Place date, latitude, and longitude into respective arrays
-	$date($cnt) = chomp($tmpArr(5)); 
-	$latitude($cnt) = chomp($tmpArr(13));
-	$longitude($cnt) = chomp($tmpArr(14));
+	$date[$cnt] = $tmpArr[5]; 
+	$latitude[$cnt] = $tmpArr[13];
+	$longitude[$cnt] = $tmpArr[14];	
+
+	chomp $date[$cnt];
+	chomp $latitude[$cnt];
+	chomp $longitude[$cnt];
 
 	#Increment counter variable
 	$cnt++;
 }	
 	
 #Separate headers from data
-for (my $count = 0; $count <= $cnt; $count++) {
+for (my $count = 0; $count < $cnt; $count++) {
 	#Remove first 15 characters to isolate data
-	$date($count) = substr($date($count), 15); 
-	$latitude($count) = substr($latitude($count), 15);
-	$longitude($count) = substr($longitude($count), 15);
+	$date[$count] = substr($date[$count], 15); 
+	$latitude[$count] = substr($latitude[$count], 15);
+	$longitude[$count] = substr($longitude[$count], 15);
 }	
+
 	
 #Convert DMS format to decimal format	
-for (my $count = 0; $count <= $cnt; $count++) {	
+for (my $count = 0; $count < $cnt; $count++) {	
 	#Temporary arrays to separate cardinal directions from coordinates
-	my $latDir = substr($latitude($count), 0, 2);
-	my $longDir = substr($longitude($count), 0, 2);
-	my $tmpLat = substr($latitude($count), 2);
-	my $tmpLong = substr($longitude($count), 2);
+	my $latDir = substr($latitude[$count], 0, 2);
+	my $longDir = substr($longitude[$count], 0, 2);
+	my $tmpLat = substr($latitude[$count], 2);
+	my $tmpLong = substr($longitude[$count], 2);
 	
+	#print "LatDir = $latDir.\nLongDir = $longDir.\nTmpLat = $tmpLat.\nTmpLong = $tmpLong.\n";
+
 	#Separate degree, minute, and second values and remove letters (d, m, s)
 	$tmpLat =~ tr/dms//d;
 	$tmpLong =~ tr/dms//d;
@@ -66,15 +73,16 @@ for (my $count = 0; $count <= $cnt; $count++) {
 	my @sepLong = split(/ /, $tmpLong);
 	
 	#Convert DMS to decimal
-	$tmpLat = ($sepLat(2) / 60 + $sepLat(1)) / 60 + $sepLat(0);
-	$tmpLong = ($sepLong(2) / 60 + $sepLong(1)) / 60 + $sepLong(0);
+	$tmpLat = ($sepLat[2] / 60 + $sepLat[1]) / 60 + $sepLat[0];
+	$tmpLong = ($sepLong[2] / 60 + $sepLong[1]) / 60 + $sepLong[0];
 	
 	#Append cardinal direction back to coordinates
-	$latitude($count) = $latDir . $tmpLat;
-	$longitude($count) = $longDir . $tmpLong;
+	$latitude[$count] = $latDir . $tmpLat;
+	$longitude[$count] = $longDir . $tmpLong;
 }	
 	
-	
+print $latitude[0] . "\n";
+print $longitude[0] . "\n";	
 	
 	
 	
